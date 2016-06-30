c6696f5f80483172984b1ee5ef9606912b5e20c9
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-30 00:12:33 -0700

    Add front end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
 //Back-End//
 var pingPong = function(number) {
-  var result;
+  var result=[];
   var index;
+  var x=[];
   for (var index = 1; index <= number; index += 1) {
     if (index % 15 === 0) {
-    result = "pingpong";
+    result.push("pingpong");
     } else if (index % 3 === 0) {
-    result = "ping";
+    result.push("ping");
     } else if (index % 5 === 0) {
-    result = "pong";
+    result.push("pong");
     } else {
-    result = index;
+    result.push(index);
     }
-    return result;
+
   }
+return result;
 };
 
 //Front-End//
 $(document).ready(function () {
   $("#blanks form").submit(function(event) {
-    event.preventDefault();
+
     // var userInput = parseInt($("input#number").val());
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
-   $("#output").append("<li>"+ result + "</li>");
-  });
+
+    if (isNaN(number)) {
+          alert("Please enter a number")
+      };
+      result.forEach(function(index) {
+        $(".uncover").append("<li>" + index + "</li>");
+      });
+      $("#output").show();
+
+      event.preventDefault();
+});
 });
